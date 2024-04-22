7790c4642abeacb2fef4d73732e0de0667aa5545
Kaifeng Zheng <kaifeng.zheng@stonybrook.edu>
2024-04-21 22:42:04 -0400

    debug

--- zanes/data_analysis.py
+++ zanes/data_analysis.py
@@ -1103,40 +1103,28 @@ class zanes_data_analysis:
         min_max = min(max_k)
         max_min_r = max(min_r)
         min_max_r = min(max_r)
-        kdata = np.round(np.linspace(max_min, min_max, 500), 4)
-        rdata = np.round(np.linspace(max_min_r, min_max_r, 500), 4)
+        kdata = np.round(np.linspace(max_min+0.001, min_max-0.001, 500), 4)
+        rdata = np.round(np.linspace(max_min_r+0.001, min_max_r-0.001, 500), 4)
         np.savetxt(join(foldername, "kdata.txt"), kdata)
         np.savetxt(join(foldername, "rdata.txt"), rdata)
         for i in range(len(dset)):
             for j in range(len(dset[i])):
-                data_k[f"{i*j+j}"] = np.round(
-                    interp1d(
+                data_k[f"{i*j+j}"] = interp1d(
                         dset[i][j].data.k,
                         dset[i][j].data.chi
                         * dset[i][j].data.k ** dset[i][j].transform.kweight,
-                    )(kdata),
-                    6,
-                )
-                data_r[f"{i*j+j}"] = np.round(
-                    interp1d(
+                    )(kdata)
+                data_r[f"{i*j+j}"] =interp1d(
                         dset[i][j].data.r,
-                        dset[i][j].data.chir_mag)(rdata),
-                    6,
-                )
-                fitted_k[f"{i*j+j}"] = np.round(
-                    interp1d(
+                        dset[i][j].data.chir_mag)(rdata)
+                fitted_k[f"{i*j+j}"] =interp1d(
                         dset[i][j].model.k,
                         dset[i][j].model.chi
                         * dset[i][j].model.k ** dset[i][j].transform.kweight,
-                    )(kdata),
-                    6,
-                )
-                fitted_r[f"{i*j+j}"] = np.round(
-                    interp1d(
+                    )(kdata)
+                fitted_r[f"{i*j+j}"] =interp1d(
                         dset[i][j].model.r,
-                        dset[i][j].model.chir_mag)(rdata),
-                    6,
-                )
+                        dset[i][j].model.chir_mag)(rdata)
         data_k_table = pd.DataFrame(data_k)
         data_r_table = pd.DataFrame(data_r)
         fitted_k_table = pd.DataFrame(fitted_k)

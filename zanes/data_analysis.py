0568d5225855ffb8ad712d885670282b380b539b
kaifengZheng <kaifeng.zheng@stonybrook.edu>
2024-07-13 20:41:51 -0400

    add

--- zanes/data_analysis.py
+++ zanes/data_analysis.py
@@ -865,10 +865,10 @@ class zanes_data_analysis:
                     paths.append(
                         feffpath(
                             f"{feff_folder[f]}/{feff_paths['file'][fitpath_num[f][j]]}",
-                            s02="abs("+s02+")",
+                            s02=s02,
                             degen=1,
                             e0=dele,
-                            sigma2="abs("+ss2+")",
+                            sigma2=ss2,
                             deltar=delr,
                         )
                     )
